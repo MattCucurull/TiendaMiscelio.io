b49eb9f989451a765934a9af12b57c6a85e27cd1
MattCucurull <139018636+MattCucurull@users.noreply.github.com>
2023-07-14 19:56:09 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+User
+320x480 (Móviles antiguos y algunos dispositivos de gama baja). COMPLETED
+360x640 (Teléfonos móviles modernos y de gama baja). COMPLETED
+375x667 (iPhone 6/7/8 y dispositivos similares). COMPLETED
+414x736 (iPhone 6/7/8 Plus y dispositivos similares en modo vertical).
+768x1024 (Tablets en modo vertical, como el iPad).
+1024x768 (Tablets en modo horizontal).
+1366x768 (Laptops y computadoras de escritorio pequeñas).
+1440x900 (Laptops y computadoras de escritorio medianas).
+1920x1080 (Pantallas de alta definición comunes). COMPLETED
+2560x1440 (Pantallas de alta resolución más grandes).
+3840x2160 (Pantallas 4K y superiores).
+You've completed the 25% of all the resolutions purposed
+*/
+
+
+function redirecToProductos(){
+  location.href="https://chat.openai.com/?model=text-davinci-002-render-sha";
+}
